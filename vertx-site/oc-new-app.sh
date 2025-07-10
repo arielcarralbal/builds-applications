dd2fda4b4e37cc7febb1e0170c39049e68b0f360
Ariel Carralbal <acarralb@redhat.com>
2025-07-10 18:20:10 -0300

    2.0

--- vertx-site/oc-new-app.sh
+++ vertx-site/oc-new-app.sh
@@ -4,7 +4,5 @@ oc new-app --name vertx-site \
   --build-env MAVEN_MIRROR_URL=http://nexus-route-nexus-infra.apps-crc.testing/repository/maven-central-proxy/ \
   --env JAVA_APP_JAR=vertx-site-1.0.0-SNAPSHOT-fat.jar \
   -i redhat-openjdk18-openshift:1.8 \
-  --context-dir builds-applications/apps/vertx-site \
-  https://github.com/RedHatTraining/DO288-apps
-
- 
+  --context-dir vertx-site \
+  https://github.com/arielcarralbal/builds-applications
